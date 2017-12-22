7531c304f4037fc56b8352234144e2e9399e8159
Alexia Le Tarnec <alexialetarnec@gmail.com>
2017-12-22 10:48:53 +0100

    started website

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,13 @@
+class PagesController < ApplicationController
+  def contact
+  end
+
+  def about
+  end
+
+  def portfolio
+  end
+
+  def home
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+    root to: 'pages#home'
+  get 'pages/contact', to: 'pages#contact', as: :contact
+
+  get 'about', to: 'pages#about', as: :about
+
+  get 'pages/portfolio', to: 'pages#portfolio', as: :portfolio
+
+  get 'pages/home', to: 'pages#home', as: :home
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
